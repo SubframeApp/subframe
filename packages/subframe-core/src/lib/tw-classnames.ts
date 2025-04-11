4a04b2f3fb09f5e6ec454e159b6d91aab768f966
Adam Ginzberg <aginzberg@subframe.com>
2025-04-11 16:18:47 -0400

    [Release] @subframe/core 1.142.0

--- packages/subframe-core/src/lib/tw-classnames.ts
+++ packages/subframe-core/src/lib/tw-classnames.ts
@@ -1,8 +1,38 @@
 import classNames from "classnames"
 import { createTailwindMerge, getDefaultConfig } from "tailwind-merge"
 
-// HACK: keep this in sync with the default fonts
-export const SUBFRAME_FONT_MIXINS = [
+export function createTwClassNames(subframeFontMixins: string[]) {
+  const customTWMerge = createTailwindMerge(() => {
+    const defaultConfig = getDefaultConfig()
+
+    return {
+      ...defaultConfig,
+      classGroups: {
+        ...defaultConfig.classGroups,
+        // "subframe-colors": [{ text: [isTextColor] }],
+        "subframe-font-mixins": subframeFontMixins,
+        // "font-size": [{ text: [isFontStyle] }, ...defaultConfig.classGroups["font-size"]],
+        // leading: [{ text: [isFontStyle] }, ...defaultConfig.classGroups.leading],
+        // "font-weight": [{ text: [isFontStyle] }, ...defaultConfig.classGroups["font-weight"]],
+      },
+      conflictingClassGroups: {
+        // font colors
+        // "text-color": ["subframe-colors"],
+
+        // font mixins
+        "subframe-font-mixins": ["font-size", "leading", "font-weight"],
+        "font-size": ["subframe-font-mixins"],
+        leading: ["subframe-font-mixins"],
+        "font-weight": ["subframe-font-mixins"],
+      },
+    }
+  })
+
+  return (...args: Parameters<typeof classNames>) => customTWMerge(classNames(...args))
+}
+
+// TODO: Remove. Going forward this is generated into a utils.ts file.
+export const twClassNames = createTwClassNames([
   // old font mixins
   "text-label",
   "text-label-bold",
@@ -19,34 +49,4 @@ export const SUBFRAME_FONT_MIXINS = [
   "text-heading-3",
   "text-heading-2",
   "text-heading-1",
-]
-
-const customTWMerge = createTailwindMerge(() => {
-  const defaultConfig = getDefaultConfig()
-
-  return {
-    ...defaultConfig,
-    classGroups: {
-      ...defaultConfig.classGroups,
-      // "subframe-colors": [{ text: [isTextColor] }],
-      "subframe-font-mixins": SUBFRAME_FONT_MIXINS,
-      // "font-size": [{ text: [isFontStyle] }, ...defaultConfig.classGroups["font-size"]],
-      // leading: [{ text: [isFontStyle] }, ...defaultConfig.classGroups.leading],
-      // "font-weight": [{ text: [isFontStyle] }, ...defaultConfig.classGroups["font-weight"]],
-    },
-    conflictingClassGroups: {
-      // font colors
-      // "text-color": ["subframe-colors"],
-
-      // font mixins
-      "subframe-font-mixins": ["font-size", "leading", "font-weight"],
-      "font-size": ["subframe-font-mixins"],
-      leading: ["subframe-font-mixins"],
-      "font-weight": ["subframe-font-mixins"],
-    },
-  }
-})
-
-export function twClassNames(...args: Parameters<typeof classNames>): string {
-  return customTWMerge(classNames(...args))
-}
+])
