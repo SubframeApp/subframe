7405e885ba265c507f781669fda7aadaa78f7c7b
Adam Ginzberg <aginzberg@subframe.com>
2025-05-22 10:08:50 -0700

    Add new package and update CLI for byoc

---
+++ packages/shared/css-helpers.ts
@@ -0,0 +1,3 @@
+export function getScopedCSSClassName(id: string) {
+  return `registry-${id}`
+}
---
+++ packages/subframe-byoc/src/types.ts
@@ -0,0 +1,114 @@
+/**
+ * Properties
+ */
+interface TextPropertyDefinition {
+  type: "text"
+  defaultValue?: string
+}
+
+interface NumberPropertyDefinition {
+  type: "number"
+  defaultValue?: number
+}
+
+interface ImagePropertyDefinition {
+  type: "image"
+  defaultValue?: string
+}
+
+interface BooleanPropertyDefinition {
+  type: "boolean"
+  defaultValue?: boolean
+}
+
+interface EnumPropertyDefinition<T extends readonly string[] = readonly string[]> {
+  type: "enum"
+  options: T
+  defaultValue?: T[number]
+}
+
+interface SlotPropertyDefinition {
+  type: "slot"
+  // TODO: should there be a defaultValue?
+}
+
+interface IconPropertyDefinition {
+  type: "icon"
+  defaultValue?: string
+}
+
+export type PropertyDefinition =
+  | TextPropertyDefinition
+  | NumberPropertyDefinition
+  | ImagePropertyDefinition
+  | BooleanPropertyDefinition
+  | EnumPropertyDefinition
+  | SlotPropertyDefinition
+  | IconPropertyDefinition
+
+// Helper type to extract the corresponding value type from a PropertyDefinition
+type PropertyValueType<T extends PropertyDefinition> = T extends TextPropertyDefinition
+  ? string
+  : T extends NumberPropertyDefinition
+  ? number
+  : T extends BooleanPropertyDefinition
+  ? boolean
+  : T extends EnumPropertyDefinition
+  ? T["options"][number]
+  : T extends SlotPropertyDefinition
+  ? React.ReactNode
+  : T extends IconPropertyDefinition
+  ? string
+  : never
+
+// Transform the properties object type into the args type
+type PropertiesArgs<T extends { [name: string]: PropertyDefinition }> = {
+  [K in keyof T]: T[K] extends { defaultValue: any } ? PropertyValueType<T[K]> : PropertyValueType<T[K]> | undefined
+}
+
+/**
+ * Import info
+ */
+
+// e.g. import Card from "@design-system/card"
+interface ImportInfoDefault {
+  type: "default"
+  packageName: string
+  property?: string // when true, then use dot notation to access the component, e.g. Card.Section
+}
+
+// e.g. import { Card } from "@design-system/card"
+interface ImportInfoNamed {
+  type: "named"
+  packageName: string
+  name: string
+  property?: string // when true, then use dot notation to access the component, e.g. Card.Section
+}
+export type ImportInfo = ImportInfoDefault | ImportInfoNamed
+
+interface IconMetadata {
+  importInfo: ImportInfo
+  render: (args: object, ref?: React.Ref<SVGSVGElement>) => React.ReactNode
+}
+
+export interface IconLibrary {
+  [name: string]: IconMetadata
+}
+
+/**
+ * Metadata
+ */
+export type MetadataProperties = { [name: string]: PropertyDefinition }
+export interface Metadata<P extends MetadataProperties = MetadataProperties, Q extends MetadataProperties[] = []> {
+  title: string
+  description?: string
+  component: React.ComponentType<any>
+  properties: P
+  subcomponents?: { [K in keyof Q]: Metadata<Q[K]> }
+  importInfo: ImportInfo
+  render: (args: PropertiesArgs<P>, ref?: React.Ref<HTMLElement>) => React.ReactNode
+}
+
+export interface ComponentLibrary {
+  [name: string]: Metadata
+}
