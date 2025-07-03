f56c5b8b471a0718a9a5b9e1844288eadf6d9b85
adam-subframe <152632622+adam-subframe@users.noreply.github.com>
2025-07-03 10:14:39 -0700

    [Release] @subframe/core 1.145.0 (#50)

--- packages/subframe-core/src/components/drawer.tsx
+++ packages/subframe-core/src/components/drawer.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as RadixDialog from "@radix-ui/react-dialog"
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden"
 import classNames from "classnames"
 import React from "react"
 import styles from "./drawer.module.css"
@@ -8,7 +9,7 @@ import styles from "./drawer.module.css"
 interface RootProps extends RadixDialog.DialogProps, RadixDialog.DialogOverlayProps {}
 
 export const Root = React.forwardRef<HTMLDivElement, RootProps>(function Drawer(
-  { className, defaultOpen, open, onOpenChange, modal, ...otherProps },
+  { className, defaultOpen, open, onOpenChange, modal, title = "", ...otherProps },
   ref,
 ) {
   return (
@@ -16,15 +17,21 @@ export const Root = React.forwardRef<HTMLDivElement, RootProps>(function Drawer(
       <RadixDialog.Portal>
         <RadixDialog.Overlay ref={ref} asChild className={classNames(className, styles.root)} {...otherProps} />
       </RadixDialog.Portal>
+      {/* Silence Dialog.Title warning: https://github.com/radix-ui/primitives/blob/dae8ef4920b45f736e2574abf23676efab103645/packages/react/dialog/src/Dialog.tsx#L506 */}
+      <VisuallyHidden.Root asChild>
+        <RadixDialog.Title>{title}</RadixDialog.Title>
+      </VisuallyHidden.Root>
     </RadixDialog.Root>
   )
 })
 
 export const Content = React.forwardRef<HTMLDivElement, RadixDialog.DialogContentProps>(function Content(
-  { ...otherProps },
+  // Silence Dialog.Description warning:
+  // https://github.com/radix-ui/primitives/blob/dae8ef4920b45f736e2574abf23676efab103645/packages/react/dialog/src/Dialog.tsx#L532
+  { "aria-describedby": ariaDescribedBy = undefined, ...otherProps },
   ref,
 ) {
-  return <RadixDialog.Content ref={ref} asChild {...otherProps} />
+  return <RadixDialog.Content aria-describedby={ariaDescribedBy} ref={ref} asChild {...otherProps} />
 })
 
 export const Drawer = { Root, Content }
