01f3127f825990d4b8a5520b29531e6110226fe9
adam-subframe <152632622+adam-subframe@users.noreply.github.com>
2025-12-08 16:28:55 -0800

    Logger updates, surface errors for codegen files (#80)

---
+++ packages/shared/logger/constants.ts
@@ -0,0 +1,6 @@
+// hardcoded by Posthog: https://posthog.com/docs/libraries/segment#using-group-analytics
+export const SEGMENT_GROUP_KEY = "segment_group"
+
+// Note: These are not secrets and fine being hardcoded in the source code.
+export const ANONYMOUS_SERVER_USER_ID = "ANONYMOUS_SERVER_USER-06576d5d-29ba-406c-8453-1aefa8d4d6b7" as const
+export const ANONYMOUS_CLI_USER_ID = "ANONYMOUS_CLI_USER-db6a3ec1-756a-4931-acdd-ec29f531603c" as const
